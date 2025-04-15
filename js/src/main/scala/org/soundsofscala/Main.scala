570785569f2f8aa07acc08d77e5ff80e6cd2fafa
paulclearscore <paul.matthews@clearscore.com>
2025-04-15 07:54:35 +0100

    SOS-000: more tidying

--- js/src/main/scala/org/soundsofscala/Main.scala
+++ js/src/main/scala/org/soundsofscala/Main.scala
@@ -49,7 +49,7 @@ object Main extends App:
         "click",
         (_: dom.MouseEvent) =>
           given audioContext: AudioContext = new AudioContext()
-          // your actions here
+          // Try out your songs/code here
           ExampleSongSampler.play().unsafeRunAndForget()
       )
 
